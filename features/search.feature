95689e9ce4168e70d84ed28e17a66965f31f4232
Евгений Корзун <89513152520@yandex.ru>
2024-06-20 17:54:41 +0300

    complete task

--- features/search.feature
+++ features/search.feature
@@ -1,24 +1,24 @@
 Feature: Cynema ticket
     Scenario: Booking 1 ticket
         Given user is on "/index.php" page
-        When user selects a day
-        When user selects a time
-        When user selects a place
+        When user selects day
+        When user selects time
+        When user selects place
         When user push the register button
         Then user sees the message "Вы выбрали билеты:"
 
     Scenario: Booking 2 tickets
         Given user is on "/index.php" page
-        When user selects a day
-        When user selects a time
-        When user selects a place 1
-        When user selects a place 2
+        When user selects day
+        When user selects time
+        When user selects place 1
+        When user selects place 2
         When user push the register button
         Then user sees the message "Вы выбрали билеты:"
 
-    Scenario: The booking button is not available
+    Scenario: The button is not available
         Given user is on "/index.php" page
-        When user selects a day
-        When user selects a time
-        When user selects a place 3
+        When user selects day
+        When user selects time
+        When user selects place 3
         Then user cant push the register button
--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -1,8 +1,16 @@
 const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
-const { Given, When, Then, Before, After } = require("cucumber");
-const { clickElement,getText } = require("../../lib/commands.js");
+const {
+    Given,
+    When,
+    Then,
+    Before,
+    After
+} = require("cucumber");
+const {
+    clickElement
+} = require("../../lib/commands.js");
 
 Before(async function() {
     const browser = await puppeteer.launch({
@@ -23,23 +31,23 @@ Given("user is on {string} page", async function(string) {
         setTimeout: 10000,
     });
 });
-When("user selects a day", async function() {
-  return clickElement(this.page, "body nav a:nth-child(2)")
+When("user selects day", async function() {
+    return clickElement(this.page, "body nav a:nth-child(2)")
 });
-When("user selects a time", async function() {
+When("user selects time", async function() {
     return clickElement(this.page, "body main section:nth-child(2) div:nth-child(3) ul li a")
 });
-When("user selects a place", async function() {
+When("user selects place", async function() {
     return clickElement(this.page, "div:nth-child(5) span:nth-child(5)");
 });
-When("user selects a place 1", async function() {
+When("user selects place 1", async function() {
     return clickElement(this.page, "div:nth-child(5) span:nth-child(5)");
 });
-When("user selects a place 2", async function() {
+When("user selects place 2", async function() {
     return clickElement(this.page, "div:nth-child(5) span:nth-child(6)");
 });
-When("user selects a place 3", async function() {
-  return clickElement(this.page, "div:nth-child(9) span:nth-child(2)");
+When("user selects place 3", async function() {
+    return clickElement(this.page, "div:nth-child(9) span:nth-child(2)");
 });
 When("user push the register button", async function() {
     return clickElement(this.page, "button.acceptin-button")
@@ -48,13 +56,13 @@ Then("user sees the message {string}", async function(expectedMessage) {
     const messageElement = await this.page.$('h2.ticket__check-title');
     const actualMessage = await messageElement.evaluate(node => node.textContent);
     expect(actualMessage).contain(expectedMessage);
-  });
+});
 Then("user cant push the register button", async function() {
-  expect(
-    String(
-        await this.page.$eval("button", (button) => { 
-            return button.disabled;
-        })
+    expect(
+        String(
+            await this.page.$eval("button", (button) => {
+                return button.disabled;
+            })
+        )
     )
-)
 });
